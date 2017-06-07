a0683970e3fab1bd4025ddb5cd0a7a425aa1d128
SarahCheng <bigbigtreeful@gmail.com>
2017-06-07 15:37:06 +0800

    會卡住,無法跑api
    result=
    google_kw[the_kw]= oceanliners
    the_kw 16
    flag undefined
    卡

--- popularscore 2.js
+++ popularscore 2.js
@@ -9,56 +9,69 @@ function sleep(milliseconds) {
     }
   }
 }
+function google_trend(google_kw,the_kw){
+		var the_flag=false;
+		var kw_sum=Number(0);
+		console.log('google_kw[the_kw]=',google_kw[the_kw]);
+		googleTrends.interestOverTime({keyword:google_kw[the_kw] , startTime: new Date('2015-02-15'),startTime: new Date('2015-03-14')}, 
+				function(err, results){
+  					if(err) console.error('there was an error!', err);
+  					else {	
+  							// console.log('the_kw',the_kw);
+  							if (results.length > 50) { 
+	  							var res_data=results.split('}');
+								for (var i = 7 ; i >4; i--) {
+		  							var a_daydata=res_data[res_data.length-i].split(',');//倒數三天的data
+		  							var the_element=a_daydata[a_daydata.length-1].split(':');
+		  							var the_value=the_element[1].split('"');
+		  							kw_sum=kw_sum+Number(the_value[1]);
+		  							console.log('the_value[1]=',the_value[1]);
+		  						}
+	  						}
+							all_fields=kw_sum;
+							console.log('all_fields=',all_fields);
+							the_flag=true;
+							console.log('the_flag',the_flag);
+							return the_flag;
+					}	
+		})
+	
+}
 function readTextFile(file)
 {
 	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
+   	rawFile.open("GET", file, false);
+    var all_fields=Number(0);
+    var allText,lines,fields,google_kw,google_kw_split,data_scores;
+    var flag=true;
     rawFile.onreadystatechange = function ()
     {
         if(rawFile.readyState === 4)
         {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
-                var allText = rawFile.responseText;
-                var lines = allText.split('\n');
-                var data_scores;
+                allText = rawFile.responseText;
+                lines = allText.split('\n');
+                
 			    for(var line = 8; line < lines.length; line++){
-			    	var fields = lines[line].split('" "');
-			    	var google_kw= [fields[3],fields[4]];// subcategory, concept, alltag
-			    	google_kw=google_kw+','+fields[10].split(' ');
-			    	google_kw=google_kw.split(',');
-			    	console.log('google_kw.length=',google_kw.length);
-			    	var all_field_sum=Number(0);
-			    	for (var the_kw = google_kw.length - 1; the_kw >= 0; the_kw--) {
-			    		var kw_sum=Number(0);
-			    		googleTrends.interestOverTime({keyword:google_kw[the_kw] , startTime: new Date('2015-02-15'),startTime: new Date('2015-03-14')}, 
-			    				function(err, results){
-				  					if(err) console.error('there was an error!', err);
-				  					else {	
-				  							if (results.length > 50) { 
-					  							var res_data=results.split('}');
-												for (var i = 7 ; i >4; i--) {
-						  							var a_daydata=res_data[res_data.length-i].split(',');//倒數三天的data
-						  							var the_element=a_daydata[a_daydata.length-1].split(':');
-						  							var the_value=the_element[1].split('"');
-						  							kw_sum=kw_sum+Number(the_value[1]);
-						  							console.log('the_value[1]=',the_value[1]);
 
-						  						}
-					  						}
-					  				// 		fs_write.open("/Users/sarahcheng/Documents/Master/2017Spring/Datamining/final_project/task2/T2-TRAIN/output.txt", 'a', 0666, function(err, fd){
-											// 	fs_write.write(fd,kw_sum + "\n");
-											// });
-											console.log('kw_sum=',kw_sum);
-									}	
-						})
-						// all_field_sum+=kw_sum;
-						// console.log('all_field_sum=',all_field_sum);
-			    	}
-			      	
-			      	// data_scores = ' '+all_field_sum;
-			      	// console.log('data_scores=',data_scores);
+			    	fields = lines[line].split('" "');
+					google_kw= fields[10].split(' ');
+					console.log('google_kw=',google_kw);
+					all_fields=Number(0);
+			    	the_kw=google_kw.length - 1;
+
+					while((the_kw>=0)){
+						if (flag) {
+							flag=false;
+							flag=google_trend(google_kw,the_kw);
+							sleep(1000);
+							the_kw--;
+							console.log('the_kw',the_kw);
+							console.log('flag',flag);
+						}
+					}
 			    }
 
             }
@@ -68,6 +81,5 @@ function readTextFile(file)
 }
 
 readTextFile('file:///Users/sarahcheng/Documents/Master/2017Spring/Datamining/final_project/task2/T2-TRAIN/output.rtf');
-
 
 
