751b540bc08cb2a61e20e49c7c95e16eb51ea074
SarahCheng <bigbigtreeful@gmail.com>
2017-06-08 09:50:35 +0800

    relatedTopics
    google_kw[the_kw]= oceanliners
    g_res= undefined
    s_value= 100
    s_all_fields= 100,"formattedValue"

--- popularscore 2.js
+++ popularscore 2.js
@@ -10,35 +10,40 @@ function sleep(milliseconds) {
   }
 }
 function google_trend(google_kw,the_kw,busy){
-		// var the_flag=false;
+		var s_all_fields=5;
 		if (!busy) {
+			
 			busy=true;
 			var kw_sum=Number(0);
 			console.log('google_kw[the_kw]=',google_kw[the_kw]);
-			googleTrends.interestOverTime({keyword:google_kw[the_kw] , startTime: new Date('2015-02-15'),startTime: new Date('2015-03-14')}, 
+			googleTrends.relatedTopics({keyword:google_kw[the_kw] , startTime: new Date('2015-02-15'),startTime: new Date('2015-03-14')}, 
 					function(err, results){
-	  					if(err) console.error('there was an error!', err);
+	  					if(err) {
+	  						console.error('there was an error!', err);
+	  						return 0;
+	  					}
 	  					else {	
 	  							
 	  							if (results.length > 50) { 
-		  							var res_data=results.split('}');
-									for (var i = 7 ; i >4; i--) {
-			  							var a_daydata=res_data[res_data.length-i].split(',');//倒數三天的data
-			  							var the_element=a_daydata[a_daydata.length-1].split(':');
-			  							var the_value=the_element[1].split('"');
-			  							kw_sum=kw_sum+Number(the_value[1]);
-			  							console.log('the_value[1]=',the_value[1]);
-			  						}
-		  						}
+		  							// var res_data=results.split('}');
+		  							var res_data=results.split('{"rankedKeyword":[');
+		  							
+		  							var s_res_data=res_data[1].split('"},"');
+		  							var s_res_value=s_res_data[1].split(':');
+		  							var s_value=s_res_value[1].split(',');
+		  							console.log('s_value=',s_value[0]);
 
-								all_fields=kw_sum;
-								console.log('all_fields=',all_fields);
-								busy=false;
-								console.log('the_flag',the_flag);
-								return busy;
+		  						}
+		  						s_all_fields=s_res_value[1];	
+								console.log('s_all_fields=',s_all_fields);
+								
+								
 						}	
+						return s_all_fields;
 			})
+			
 		}
+		
 }
 function readTextFile(file)
 {
@@ -47,7 +52,7 @@ function readTextFile(file)
    	rawFile.open("GET", file, false);
     var all_fields=Number(0);
     var allText,lines,fields,google_kw,google_kw_split,data_scores;
-    var flag=true;
+    var flag;
     rawFile.onreadystatechange = function ()
     {
         if(rawFile.readyState === 4)
@@ -57,20 +62,16 @@ function readTextFile(file)
                 allText = rawFile.responseText;
                 lines = allText.split('\n');
                 
-			    for(var line = 8; line < lines.length; line++){
+			    	var line = 8;
 
 			    	fields = lines[line].split('" "');
 					google_kw= fields[10].split(' ');
 					console.log('google_kw=',google_kw);
 			    	all_fields=Number(0);
 					the_kw=google_kw.length - 1;
-					var isbusy=true;
-					isbusy=google_trend(google_kw,the_kw,flag);
-					the_kw--;
-					console.log('the_kw',the_kw);
-					
-
-			    }
+					var isbusy=false;
+					var g_res = google_trend(google_kw,the_kw,isbusy);
+					console.log('g_res=',g_res);
 
             }
         }
@@ -79,6 +80,3 @@ function readTextFile(file)
 }
 
 readTextFile('file:///Users/sarahcheng/Documents/Master/2017Spring/Datamining/final_project/task2/T2-TRAIN/output.rtf');
-
-
-
