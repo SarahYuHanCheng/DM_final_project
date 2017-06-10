b7404eb980741bbcb0aea9406b8b6388b039a69e
SarahCheng <bigbigtreeful@gmail.com>
2017-06-10 18:53:32 +0800

    system call 一筆 ok
    head: subcategory concept 順序OK

--- popularscore 2.js
+++ popularscore 2.js
@@ -1,29 +1,34 @@
 var myArgs = process.argv.slice(2);
 
-var google_kw=myArgs.slice(0,myArgs.length-1);
+var head=myArgs.slice(0,2);
+var tags=myArgs.slice(2,myArgs.length-1);
 
 var postdate = new Date((myArgs[myArgs.length-1]-72000)*1000);
 var postdate_3 = new Date((myArgs[myArgs.length-1]-216000)*1000);
 
 const fs_write = require('fs');
 const check = require('fs');
-
+var in_cs=1;
 var tag_score=0;
+var head_score='head=';
 var t_count=0;
+var count=0;
+var temp=-1;
 main();
 
+
+function tag_google_trend(google_kw,the_kw,postday_3,postday,fd_t){
+
 		
-function main(){
-		
-		fs_write.open("/Users/sarahcheng/Documents/Master/2017Spring/Datamining/final_project/task2/T2-TRAIN/output.txt", 'a', 0666, 
-    		function(err, fd_t){
-				
-				var the_kw=0;					
-				var googleTrends = require('google-trends-api');
-		
+		in_cs=1;
+		var googleTrends = require('google-trends-api');
+		var s_all_fields=0;
+		// if (!busy) {
+			
+			// busy=true;
 			var kw_sum=Number(0);
 			
-			googleTrends.interestOverTime({keyword:google_kw[the_kw], startTime: postdate_3 ,endTime: postdate}, 
+			googleTrends.interestOverTime({keyword:google_kw[the_kw], startTime: postday_3 ,endTime: postday}, 
 					function(err, results){
 
 	  					if(err) {
@@ -43,19 +48,107 @@ function main(){
 			  							
 			  						}
 			  						t_count++;
-		  						}
+		  						}else{t_count++;}
+								tag_score=tag_score+Number(kw_sum);
+								kw_sum=0;
 								
-								if (t_count==1) {
-									fs_write.write(fd_t,kw_sum+'\n');
+								if (t_count==google_kw.length) {
+									fs_write.write(fd_t,'  alltags='+tag_score);
 							 		check.open("/Users/sarahcheng/Desktop/check.txt",'w',0666,function(err, fd_check){
 						    		check.write(fd_check,"OK");
 									});
 
-							}	
+							}
+															
+								
 						}	
+						
+
 			})
+			
+		// }
+		// s_all_fields=1;
+		return 1;
+		
+}
+function main(){
+
+	    // do{
+    	fs_write.open("/Users/sarahcheng/Documents/Master/2017Spring/Datamining/final_project/task2/T2-TRAIN/output.txt", 'a', 0666, 
+    		function(err, fd){
+				var h_the_kw=0;
+				
+				do {
+						google_trend(head,h_the_kw,postdate_3,postdate,fd);
+						h_the_kw++;
+
+					}while(head.length >h_the_kw)	
+
+				the_kw=0;
+				do {
+					tag_google_trend(tags,the_kw,postdate_3,postdate,fd);
+					the_kw++;
+				}while(tags.length >the_kw)	
+				fs_write.write(fd,"\n");
 
 			});
+   
+		// }while()
 }
+
+	
+function google_trend(google_kw_h,the_kw_h,postday_3,postday,fd_h){
+
+		var googleTrends_h = require('google-trends-api');
+		var kw_sum_h=Number(0);
+
+		
+		googleTrends_h.interestOverTime({keyword:google_kw_h[the_kw_h], startTime: postday_3 ,endTime: postday}, 
+				function(err, results_h){
+
+  					if(err) {
+  						console.error('there was an error!', err);
+  						return 0;
+  					}
+  					else {	
+  							
+  							if (results_h.length > 50) { 
+  								var res_data_h=results_h.split('}');
+	  							
+								for (var i = 7 ; i >4; i--) {
+		  							var a_daydata_h=res_data_h[res_data_h.length-i].split(',');//倒數三天的data
+		  							var the_element_h=a_daydata_h[a_daydata_h.length-1].split(':');
+		  							var the_value_h=the_element_h[1].split('"');
+		  							kw_sum_h=kw_sum_h+Number(the_value_h[1]);
+		  						
+		  						}
+		  						if (the_kw_h==1 && count==0) {
+									temp=kw_sum_h;
+
+								}else{
+									head_score=head_score+' '+kw_sum_h;	
+								}
+								count++;
+
+		  						if (count==2) {
+		  							if (temp>0) {
+		  								head_score=head_score+' '+temp;
+		  							}
+		  							fs_write.write(fd_h,'  '+head_score );
+		  						}
+	  						}
+							
+						kw_sum_h=0;
+														
+							
+					}	
+					
+		})
+		return 1;
+		
+}
+
+
+
 
 
